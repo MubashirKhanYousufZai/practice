c28e930b7d7051b83490e16189c2dadfd2f12bc5
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-03-06 21:27:42 +0500

    todays practice

---
+++ main.ts
@@ -0,0 +1,43 @@
+ let favouriteColor = "blue";
+console.log(favouriteColor)
+ favouriteColor = "green";
+ console.log(favouriteColor);
+ let userName :string ="MunashirKhan";
+ let age : number = 15;
+ let isStudent: boolean =false;
+
+let a : string ="Pakistan";
+let b : number =8;
+let c : boolean = true;
+
+console.log(a,b,c);
+let mySalary : number = 0
+console.log(mySalary) 
+let khan = "MubashirYousufzai";
+let here = "Hello ";
+console.log(here+khan);
+ let num1 =100;
+  let str1 =100;
+ let num2 =90;
+  let str2 =90;
+ let num3 =80;
+  let str3 =80;
+ let num4 =70;
+  let str4 =70;
+ let num5 =60;
+  let str5 =60;
+ let num6 =50;
+  let str6 =50;
+ let num7 =40;
+  let str7 =40;
+ let num8 =30;
+  let str8 =30;
+ let num9 =20;
+  let str9 =20;
+ let num10 =10;
+  let str10 =10;
+ let str11=2;
+
+ console.log(num1+num2+num3+num4+num5+num6+num7+num8+num9+num10);
+ console.log(str1+str2+str3+str4+str5+str6+str7+str8+str9+str10);
+
